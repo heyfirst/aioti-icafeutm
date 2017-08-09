312b527dbd69eee828ac48e2ca0f2b8918e4c271
Kanisorn S <firstziiz.k@gmail.com>
2017-08-09 10:51:01 +0800

    completed

--- kitchen-site/src/App.js
+++ kitchen-site/src/App.js
@@ -1,44 +1,129 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+var NotificationSystem = require('react-notification-system');
+
+import { compose, withState, withHandlers } from 'recompose'
+
+/* global firebase */
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      order: [],
+      foods: []
+    }
+  }
+
+  async componentWillMount() {
+    await firebase.database().ref().child('/orders')
+    .orderByKey().limitToLast(500)
+      .on('value', snapshot => {
+        this.setState({ order: snapshot.val() })
+      })
+    await firebase.database().ref().child('/foods')
+      .once('value').then(snapshot => {
+        let raw = snapshot.val()
+        let foods = Object.keys(raw)
+        .reverse()
+        .map(key => ({
+          key,
+          data: raw[key]
+        }))
+        this.setState({ foods })
+      })
+  }
+
   render() {
-    return (
-      <div className="App">
-        <nav className="navbar navbar-light bg-faded">
-          <a className="navbar-brand" href="#">iCafe @ UTM</a>
-        </nav>
-        <h1 className="text-center">Kitchen Dashboard</h1>
-        <div className="container">
-          <div className="row">
-            <div className="col-4">
-              <div className="card">
-                <ul className="list-group list-group-flush">
-                  <a href="#" className="list-group-item">Cras justo odio</a>
-                  <a href="#" className="list-group-item">Dapibus ac facilisis in</a>
-                  <a href="#" className="list-group-item">Vestibulum at eros</a>
-                </ul>
-              </div>
+    return <KitchenCompose orderList={this.state.order} foods={this.state.foods}/>;
+  }
+}
+
+const Kitchen = (props) => {
+  if(props.orderList === null) {
+    return <div />
+  }
+  const order = Object.keys(props.orderList)
+    .reverse()
+    .map(key => ({
+      key,
+      data: props.orderList[key]
+    }))
+  return (
+    <div className="App">
+      <nav className="navbar navbar-light bg-faded">
+        <a className="navbar-brand" href="#">iCafe @ UTM</a>
+      </nav>
+      <h1 className="text-center">Kitchen Dashboard</h1>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-6">
+            <div className="card">
+              <ul className="list-group list-group-flush">
+                {
+                  order.map(o => (
+                    <a
+                      key={o.key}
+                      href="#"
+                      className={"list-group-item " + (props.orderKey === o.key ? 'active' : '')}
+                      onClick={() => props.setOrderKey(o.key)}
+                    >
+                      {`ORDER:  ${o.key} `}
+
+                      {
+                        o.data.payment && (<span className="badge badge-success">PAY</span>)
+                      }
+                    </a>
+                  ))
+                }
+              </ul>
             </div>
-            <div className="col-8">
-              <div className="card">
-                <div className="card-block text-left">
-                  <h4 className="card-title">Table 1</h4>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias consequuntur consequatur totam.</p>
-                </div>
-                <ul className="list-group list-group-flush">
-                  <li href="#" className="list-group-item">Cras justo odio</li>
-                  <li href="#" className="list-group-item">Dapibus ac facilisis in</li>
-                  <li href="#" className="list-group-item">Vestibulum at eros</li>
-                </ul>
-              </div>
-            </div>
+          </div>
+          <div className="col-6">
+            <Order order={order.find(o => o.key === props.orderKey)} foods={props.foods}/>
           </div>
         </div>
       </div>
-    );
+    </div>
+  )
+}
+
+const KitchenCompose = compose(
+  withState('orderKey','setOrderKey',''),
+)(Kitchen)
+
+const Order = props => {
+  if( props.order === undefined) {
+    return <div />
   }
+
+  let order = props.order
+  return (
+    <div className="card">
+      <div className="card-block text-left">
+        <h3 className="card-title">{order.data.table}</h3>
+        <p>just a table on the iCafe : )</p>
+      </div>
+      <ul className="list-group list-group-flush">
+        {
+          order.data.menus.map(m => {
+            return (<li key={m.food} href="#" className="list-group-item">{m.food} x {m.quantity}</li>)
+          })
+        }
+      </ul>
+      <div className="card-block">
+        <h5 className="text-right">
+          {'Total price: RM'}
+          {
+            Math.round(order.data.menus.reduce( (sum,m) => {
+                return sum + props.foods.find(f => m.food === f.data.name).data.price * m.quantity
+              }, 0) * 100) / 100
+          }
+        </h5>
+      </div>
+    </div>
+  )
 }
 
 export default App;
